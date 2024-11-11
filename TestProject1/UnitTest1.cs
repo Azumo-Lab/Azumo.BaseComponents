ad7b55f40d38b98b98a70b6b641a6400371716ac
小莕菜 <31852138+sokushu@users.noreply.github.com>
2024-11-11 11:20:16 +0800

    移除无用的测试类和方法
    删除了 `UnitTest1` 类中的 `TestMethod1` 方法的所有代码，包括创建和执行管道的代码，以及对结果的断言和配置文件的操作。
    删除了 `TestProcess01` 和 `TestProcess02` 类，这两个类实现了 `IPipelineProcess<TestResult>` 接口，并在 `ExecuteAsync` 方法中对 `TestResult` 进行了处理。
    删除了 `TestResult` 类，该类实现了 `IPipelineResult` 接口，并包含一个 `Result` 属性。

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -9,43 +9,7 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            var pipelineBuilder = PipelineBuilder<TestResult>.Create();
-            pipelineBuilder.AddProcess(new TestProcess01());
-            pipelineBuilder.AddProcess(new TestProcess02());
-            var pipeline = pipelineBuilder.BuildPipeline();
-
-            var result = new TestResult();
-
-            pipeline.ExecuteAsync(result);
-
-            Assert.AreEqual("TestProcess01TestProcess02", result.Result);
-
-            SimpleConfigurationFile.Initialize();
-            SimpleConfigurationFile.Write("TestResult", result.Result);
-            SimpleConfigurationFile.SaveChange();
+            
         }
-    }
-
-    public class TestProcess01 : IPipelineProcess<TestResult>
-    {
-        public Task ExecuteAsync(TestResult input, PipelineDelegate<TestResult> Next)
-        {
-            input.Result += "TestProcess01";
-            return Next(input);
-        }
-    }
-
-    public class TestProcess02 : IPipelineProcess<TestResult>
-    {
-        public Task ExecuteAsync(TestResult input, PipelineDelegate<TestResult> Next)
-        {
-            input.Result += "TestProcess02";
-            return Next(input);
-        }
-    }
-
-    public class TestResult : IPipelineResult
-    {
-        public string Result { get; set; } = string.Empty;
     }
 }
